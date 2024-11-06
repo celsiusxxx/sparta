f5d549465cdc82430d7dbb2eb4885aeb93bd9914
celsius <celsius_x@hotmail.com>
2024-11-06 15:58:30 +0100

    Updated setup.py installation file

--- setup.py
+++ setup.py
@@ -14,34 +14,28 @@ class CustomInstallCommand(install):
         SPARTA_DIR = os.path.join("/home/kali/tools/sparta")
         VENV_DIR = os.path.join(SPARTA_DIR, "spartaEnv")
 
-        # # Step 1: Remove previous installation if it exists
-        # if os.path.exists(SPARTA_DIR):
-        #     subprocess.run(["rm", "-rf", SPARTA_DIR], check=True)
-        #     print(f"{BLUE}[+] Removed any previous installation{RESET}")
-
-        # # Step 2: Clone the Sparta repository
-        # subprocess.run(["git", "clone", "https://github.com/celsiusxxx/sparta.git", SPARTA_DIR], check=True)
-        # print(f"{BLUE}[+] Sparta GitHub repository cloned{RESET}")
-
-        # Step 3: Create virtual environment
+        # Step 1: Create virtual environment
         subprocess.run(["/usr/bin/env", "python", "-m", "venv", VENV_DIR], check=True)
         print(f"{BLUE}[+] Sparta virtual environment created{RESET}")
 
-        # Step 4: Activate the virtual environment and install requirements
+        # Step 2: Activate the virtual environment and install requirements
         subprocess.run([f"{VENV_DIR}/bin/pip", "install", "-r", f"{SPARTA_DIR}/requirements.txt"], check=True)
         print(f"{BLUE}[+] Pip requirements installed{RESET}")
+
+        # Step 3: make sparta available system-wide
+        subprocess.run(['sudo', 'bash', '-c', "echo '#!/bin/bash' > /usr/bin/sparta"], check=True)
+        subprocess.run(['sudo', 'bash', '-c', "echo 'cd /home/kali/tools/sparta/ && /usr/bin/env python3 sparta.py' >> /usr/bin/sparta"], check=True)
+        print(f"{GREEN}[+] sparta tool now available system-wide by typing : sparta{RESET}")
 
         # Final instruction message
         print(f"\n{GREEN}Installation complete!{RESET}\n")
         print("To start using Sparta, follow these instructions:\n")
-        print(f"1. {GREEN}Navigate to the Sparta directory:{RESET}")
-        print(f"   cd {SPARTA_DIR}\n")
-        print(f"2. {GREEN}Activate the virtual environment:{RESET}")
-        print(f"   source {VENV_DIR}/bin/activate\n")
-        print(f"3. {GREEN}Run the Sparta script:{RESET}")
-        print(f"   python sparta.py\n")
-        print(f"4. {GREEN}When done, deactivate the virtual environment:{RESET}")
-        print("   deactivate\n")
+        print(f"1. {GREEN}Navigate to the Sparta directory:{RESET} cd {SPARTA_DIR}\n")
+        print(f"2. {GREEN}Activate the virtual environment:{RESET} source {VENV_DIR}/bin/activate\n")
+        print(f"3. {GREEN}Run the Sparta script directly from the directory:{RESET} python sparta.py\n")
+        print(f"3. {GREEN}OR"}
+        print(f"3. {GREEN}Run sparta to keep running and freeing your terminal for other commands, use nohup::{RESET} nohup sparta > sparta.log 2>&1 &\n")
+        print(f"4. {GREEN}When done, deactivate the virtual environment:{RESET} deactivate\n")
 
 # Setup configuration
 setup(
